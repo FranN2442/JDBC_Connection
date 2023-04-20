3e71961b6203b463c7e48d1257379eeb6e8d3db9
FranN2442 <fgonzalezviejo@cifpfbmoll.eu>
2023-04-20 18:58:18 +0200

    Created Maven Project

---
+++ src/main/java/ejercicio/bbdd/App.java
@@ -0,0 +1,13 @@
+package ejercicio.bbdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
